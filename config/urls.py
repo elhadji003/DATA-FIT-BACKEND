077900ea50ede1b2c6d7153696094e2745d34627
elhadji003 <malickelhadji07@gmail.com>
2025-09-19 21:54:38 +0000

    new retest deploy

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
 
+def home(request):
+    return JsonResponse({"message": "Bienvenue sur l’API Data-Fit 🚀"})
+
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
 ]
