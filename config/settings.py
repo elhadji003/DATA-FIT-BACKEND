b04e4d105bdbc03eaa45f9f8c239ba1a4dd776f1
elhadji003 <malickelhadji07@gmail.com>
2025-09-19 23:05:11 +0000

    re new deploy

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ if "test" in os.sys.argv:
 else:
     DATABASES = {
         "default": dj_database_url.config(
-            default=os.getenv("NEON_DATABASE"),
+            default=os.getenv("DATABASE_URL"),
             conn_max_age=600,
             ssl_require=True
         )
