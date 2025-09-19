be5f06ddb78a2d44e919ff2e5d250556ac976b5c
elhadji003 <malickelhadji07@gmail.com>
2025-09-19 21:59:51 +0000

    new redploy

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home(request):
     return JsonResponse({"message": "Bienvenue sur l’API Data-Fit 🚀"})
@@ -9,4 +11,7 @@ urlpatterns = [
     path("", home),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
